14ec64a326df3a76158416b70324ad689ead4983
netclic <fc193@hotmail.com>
2024-04-18 11:54:54 +0200

    Add scheme type menu

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from kivy.utils import hex_colormap
 
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.app import MDApp
+from materialyoucolor.utils.platform_utils import SCHEMES
 
 
 def rgba_color_to_hex(color):
@@ -67,6 +68,7 @@ class KivyMDColors(MDApp):
         menu_items = []
         for item, method in {
             "Set palette": lambda: self.set_palette(),
+            "Set scheme type": lambda: self.set_scheme_type(),
             "Switch theme style": lambda: self.theme_switch(),
         }.items():
             menu_items.append({"text": item, "on_release": method})
@@ -95,9 +97,36 @@ class KivyMDColors(MDApp):
             items=menu_items,
         ).open()
 
+    def set_scheme_type(self):
+        instance_from_menu = self.get_instance_from_menu("Set scheme type")
+
+        menu_items = []
+        for scheme_name in SCHEMES.keys():
+            menu_items.append(
+                {
+                    "text": scheme_name,
+                    "on_release": lambda x=scheme_name: self.update_scheme(x),
+                }
+            )
+
+        self.scheme_menu = MDDropdownMenu(
+            caller=instance_from_menu,
+            items=menu_items,
+            width=400,
+            max_height=590,
+            hor_growth="left",
+            ver_growth="down"
+        )
+        self.scheme_menu.open()
+
     def switch_palette(self, selected_palette):
         self.theme_cls.primary_palette = selected_palette
         Clock.schedule_once(self.generate_cards, 0.5)
+
+    def update_scheme(self, scheme_name):
+        self.theme_cls.dynamic_scheme_name = scheme_name
+        Clock.schedule_once(self.generate_cards, 0.5)
+        # self.scheme_menu.dismiss()
 
     def theme_switch(self) -> None:
         self.theme_cls.switch_theme()
@@ -105,6 +134,7 @@ class KivyMDColors(MDApp):
 
     def generate_cards(self, *args):
         self.root.ids.card_list.data = []
+        self.root.ids['palette_name'].text = self.theme_cls.primary_palette + ' / ' + self.theme_cls.dynamic_scheme_name
 
         color_attributes = [attr_name for attr_name in dir(self.theme_cls) if 'Color' in attr_name]
 
@@ -120,7 +150,10 @@ class KivyMDColors(MDApp):
 
     def on_start(self):
         super().on_start()
+        self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Antiquewhite"
+        self.theme_cls.dynamic_scheme_name = "TONAL_SPOT"
+
         self.theme_cls.dynamic_color = False
         Clock.schedule_once(self.generate_cards)
 
